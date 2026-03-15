ee7c567fc1e158781723dc941114baa85558eec5
Ioan Ng <ioanng0765@gmail.com>
2026-03-15 22:49:16 +0000

    ring stuff

--- my_first_project/MyFirstProject/MoreRings.lean
+++ my_first_project/MyFirstProject/MoreRings.lean
@@ -10,6 +10,7 @@ theorem add_neg_cancel (a : R) : a + -a = 0 := by rw [add_comm, neg_add_cancel]
 #check add_zero
 theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
   rw[add_right_comm,add_assoc,neg_add_cancel,add_zero]
+--practice thing
 theorem add_left_cancel_calc {a b c : R} (h : a + b = a + c) : b = c :=
   calc
     b = 0 + b        := by rw [zero_add]
